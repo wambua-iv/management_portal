36860ca4139ff5654afd80cb896922d783bb3403
Musalu Wambua <musaluwambua@gmail.com>
2022-02-02 13:07:19 +0300

    user database schema and validtion functionality

---
+++ models/users.models.ts
@@ -0,0 +1,34 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+const userSchema = mongoose.Schema
+
+
+const usersModel = new userSchema(
+    {
+        names : {
+            firstName: { type : String, required : true },
+            lastName: { type : String, required : true },
+                },
+        user_id: {type: String, required: true},
+        email: { type: String, unique: true },
+        password: { type: String, required: true, minlength: 8 },
+        accountType: { type: String }, //student, Lecturer, Admin
+        verified: {type: Boolean, default: false }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Users = mongoose.models.Users_1 || mongoose.model('Users_1', usersModel);
+
+const validate = (User: any) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(255).required(),
+    lastName: Joi.string().min(3).max(255).required(),
+    email: Joi.string().email().required(),
+    pasaword: Joi.string().min(8).max(255).required(),
+
+  });
+  return schema.validate(User);
+};
